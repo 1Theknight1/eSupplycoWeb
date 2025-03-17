6594d5b2429671d4755c47203055e9249dde04f7
Basil Baby <bazilbaby92@gmail.com>
2025-03-17 20:44:35 +0530

    Updated firebase config to use env variables

--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -1,19 +1,7 @@
 /* eslint-disable */
 require('dotenv').config();
 const admin = require('firebase-admin');
-let serviceAccount;
-
-try {
-    if (process.env.FIREBASE_CREDENTIALS) {
-        const decodedKey = Buffer.from(process.env.FIREBASE_CREDENTIALS, 'base64').toString('utf8');
-        serviceAccount = JSON.parse(decodedKey);
-    } else {
-        serviceAccount = require("../../key.json"); // Fallback for local development
-    }
-} catch (error) {
-    console.error("❌ Error parsing Firebase credentials:", error.message);
-    process.exit(1); // Exit the app if credentials are invalid
-}
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
 
 admin.initializeApp({
