5be1939a3bb2f3fcc7391d8c7c2f6c644878acc5
Basil Baby <bazilbaby92@gmail.com>
2025-03-17 20:38:24 +0530

    Fixed Firebase credentials parsing issue

--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -1,7 +1,19 @@
 /* eslint-disable */
 require('dotenv').config();
 const admin = require('firebase-admin');
-const serviceAccount = require('../../key.json');
+let serviceAccount;
+
+try {
+    if (process.env.FIREBASE_CREDENTIALS) {
+        const decodedKey = Buffer.from(process.env.FIREBASE_CREDENTIALS, 'base64').toString('utf8');
+        serviceAccount = JSON.parse(decodedKey);
+    } else {
+        serviceAccount = require("../../key.json"); // Fallback for local development
+    }
+} catch (error) {
+    console.error("❌ Error parsing Firebase credentials:", error.message);
+    process.exit(1); // Exit the app if credentials are invalid
+}
 
 
 admin.initializeApp({
