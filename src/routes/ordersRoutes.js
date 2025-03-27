05a649848d61b7376d5d4e998d062b51277f7949
Basil Baby <bazilbaby92@gmail.com>
2025-03-27 22:15:24 +0530

    added delivery orders states

--- src/routes/ordersRoutes.js
+++ src/routes/ordersRoutes.js
@@ -1,17 +1,18 @@
 /* eslint-disable */
 
-const express=require("express")
-const {placeOrder,calculateDiscount,getOrdersByNumber,cancelOrder,getActiveOrdersByNumber,getHistory,changeOrderStatue,assignDelivery}=require("../../src/controllers/ordersController")
-const{authenticateUser}=require("../../src/middlewares/authMiddleware")
-const router=express.Router()
+const express=require("express");
+const {placeOrder,calculateDiscount,getOrdersByNumber,cancelOrder,getActiveOrdersByNumber,getHistory,changeOrderStatue,assignDelivery,outForDelivery}=require("../../src/controllers/ordersController");
+const{authenticateUser}=require("../../src/middlewares/authMiddleware");
+const router=express.Router();
 
-router.post("/orders",placeOrder)
-router.get("/:cardNumber",getOrdersByNumber)
-router.post("/calculate-discount",authenticateUser,calculateDiscount)
-router.get("/cancelOrder/:orderId",authenticateUser,cancelOrder)
-router.get("/activeOrders/:cardNumber",getActiveOrdersByNumber)
-router.get("/bookings/:cardNumber",getHistory)
-router.patch("/status/:orderId",changeOrderStatue)
-router.post("/assignDelivery",assignDelivery)
+router.post("/orders",placeOrder);
+router.get("/:cardNumber",getOrdersByNumber);
+router.post("/calculate-discount",authenticateUser,calculateDiscount);
+router.get("/cancelOrder/:orderId",authenticateUser,cancelOrder);
+router.get("/activeOrders/:cardNumber",getActiveOrdersByNumber);
+router.get("/bookings/:cardNumber",getHistory);
+router.patch("/status/:orderId",changeOrderStatue);
+router.post("/assignDelivery",assignDelivery);
+router.post("/changeDeliveryOrderStatus",outForDelivery);
 
 module.exports=router
