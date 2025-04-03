ead1b48260f58f012bd9c72f852e79996dc8f74a
Basil Baby <bazilbaby92@gmail.com>
2025-04-03 11:50:41 +0530

    added feedback api

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,13 +1,12 @@
 /* eslint-disable */
 
 const express=require("express")
-const {productsAdd,supplycoAdd,supplycoAll,deleteUser,sendMessage,setReminder,setQuota,staffRequests,staffUpdateStatus,adminDashboard,addPrimaryAdmin,getAllLogs}=require("../../src/controllers/adminController")
+const {productsAdd,supplycoAll,deleteUser,sendMessage,setReminder,setQuota,staffRequests,staffUpdateStatus,adminDashboard,addPrimaryAdmin,getAllLogs}=require("../../src/controllers/adminController")
 const {authenticateUser}=require("../../src/middlewares/authMiddleware")
 
 const router=express.Router()
 
 router.post("/products/add",productsAdd)
-router.post("/supplyco/add",supplycoAdd)
 router.get("/supplyco/all",supplycoAll)
 router.post("/userDel",deleteUser)
 router.post("/send",sendMessage)
